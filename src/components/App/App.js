4bdffcd8417f6b25a7a30cb67e6df031fb15c6de
jona872 <jona_872@hotmail.com>
2022-09-30 10:11:55 -0300

    full ux

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -54,9 +54,11 @@ function App() {
           :
           <>
             <div className="App--logo--container">
-              <img src={l2} className="App--logo" alt="fav lo" />
+              <img src={l2} className="App--logo" alt="fav logo" />
             </div>
+
             <h1>How did we do?</h1>
+            
             <span>
               Please let us know how we did with your support request. All feedback is appreciated
               to help us improve our offering!
@@ -66,10 +68,8 @@ function App() {
               {qualifications}
             </div>
 
-            <button className="card--button"
-              onClick={handleSubmit}
-            >
-              Submit
+            <button className="card--button" onClick={handleSubmit} >
+              SUBMIT
             </button>
           </>
       }
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -3,13 +3,11 @@ import successLogo from '../../assets/images/illustration-thank-you.svg';
 
 export default function Result(props) {
   return (
-    <div>
-
+    <div className="result">
       <img src={successLogo} alt="Logo sucess" />
 
-      <div>You selected {props.value} out of 5</div>
-
-
+      <div className="result--select">You selected {props.value} out of 5</div>
+    
       <h1>Thank you!</h1>
       <span>
         We appreciate you taking the time to give a rating. If you ever need more support,
